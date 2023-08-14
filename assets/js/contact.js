2398a1ce42e49ff8346864ddeeec190af6cec1e8
José AZÓCAR <pinesamo@gmail.com>
2023-08-14 16:29:39 -0400

    Correciones al coódigo JS y HTML

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,19 @@
-import { validarCampos } from "./validations.js";
-import { activarButton } from "./submit.js";
+import { validarCampos, validarButton } from "./validations.js";
 
+const form = document.querySelector('.contact__form');
 const inputs = document.querySelectorAll("[data-tipo]");
 const button = document.querySelector('.contact__form__button');
 
 inputs.forEach(input => {
     input.addEventListener('blur', (input) => {
         validarCampos(input.target);
-        activarButton(inputs, button);
+        validarButton(inputs, button);
     })
-})
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.reset();
+    validarButton(inputs, button);
+    alert("Los datos fueron enviados ... \nmuchas gracias por su interés en contactarme.");
+});
--- assets/js/validations.js
+++ assets/js/validations.js
@@ -14,6 +14,24 @@ export function validarCampos(input) {
     }
 }
 
+export function validarButton(inputs, button) {
+    let camposValidos = 0;
+
+    inputs.forEach(input => {
+        if (input.validity.valid) {
+            camposValidos++;
+        };
+    });
+
+    if (camposValidos == 4) {
+        button.disabled = false;
+        button.classList.add('contact__form__button--enabled')
+    } else {
+        button.disabled = true;
+        button.classList.remove('contact__form__button--enabled')
+    }
+};
+
 const validadores = {
     mensaje: (input) => validarMensaje(input)
 }
