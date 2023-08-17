3d2513eb3d17a708ee65d0b9340e2b7b35fc8de1
José AZÓCAR <pinesamo@gmail.com>
2023-08-17 14:24:24 -0400

    Se le agregan comentarios a los JS

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,19 +1,25 @@
+// Importar las funciones de validación desde validations.js
 import { validarCampos, validarButton } from "./validations.js";
 
+// Seleccionar el formulario y los elementos de entrada
 const form = document.querySelector('.contact__form');
 const inputs = document.querySelectorAll("[data-tipo]");
 const button = document.querySelector('.contact__form__button');
 
+// Agregar eventos de escucha a los campos de entrada
 inputs.forEach(input => {
-    input.addEventListener('blur', (input) => {
-        validarCampos(input.target);
-        validarButton(inputs, button);
+    input.addEventListener('blur', (inputEvent) => {
+        validarCampos(inputEvent.target); // Llamar a la función de validación de campos
+        validarButton(inputs, button); // Actualizar el estado del botón
     })
 });
 
+// Agregar un evento de escucha al formulario para prevenir el envío por defecto
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    form.reset();
-    validarButton(inputs, button);
-    alert("Los datos fueron enviados ... \nMuchas gracias por tu interés en contactarme.");
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+    form.reset(); // Restablecer el formulario después del envío
+    validarButton(inputs, button); // Actualizar el estado del botón
+    alert("Los datos fueron enviados ...\nMuchas gracias por tu interés en contactarme.");
 });
+
+// validations.js se encarga de exportar las funciones necesarias
--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,67 +1,76 @@
+// Exportar función para validar campos de entrada
 export function validarCampos(input) {
     const tipoDeInput = input.dataset.tipo;
 
     if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input);
+        validadores[tipoDeInput](input); // Llamar a la función de validación específica
     }
 
+    // Actualizar clases y mensajes de error en función de la validez del campo
     if (input.validity.valid) {
         input.classList.remove('contact__field__input--invalid');
-        input.parentElement.querySelector('.contact__field__error').innerHTML = ""
+        input.parentElement.querySelector('.contact__field__error').innerHTML = "";
     } else {
         input.classList.add('contact__field__input--invalid');
-        input.parentElement.querySelector('.contact__field__error').innerHTML = mostrarMensajeDeError(tipoDeInput, input)
+        input.parentElement.querySelector('.contact__field__error').innerHTML = mostrarMensajeDeError(tipoDeInput, input);
     }
 }
 
+// Exportar función para validar y habilitar/deshabilitar el botón
 export function validarButton(inputs, button) {
     let camposValidos = 0;
 
+    // Contar los campos de entrada válidos
     inputs.forEach(input => {
         if (input.validity.valid) {
             camposValidos++;
-        };
+        }
     });
 
-    if (camposValidos == 4) {
+    // Habilitar o deshabilitar el botón según el número de campos válidos
+    if (camposValidos === 4) {
         button.disabled = false;
-        button.classList.add('contact__form__button--enabled')
+        button.classList.add('contact__form__button--enabled');
     } else {
         button.disabled = true;
-        button.classList.remove('contact__form__button--enabled')
+        button.classList.remove('contact__form__button--enabled');
     }
-};
-
-const validadores = {
-    mensaje: (input) => validarMensaje(input)
 }
 
+// Objeto que contiene funciones de validación específicas para diferentes tipos de campo
+const validadores = {
+    mensaje: (input) => validarMensaje(input)
+};
+
+// Objeto que almacena los mensajes de error para diferentes tipos de campo y errores
 const mensajesDeError = {
     nombre: {
         valueMissing: 'El nombre no puede estar vacío.',
-        patternMismatch: 'El nombre solo tiene espacios en blanco.'
+        patternMismatch: 'El nombre solo puede contener letras y espacios.'
     },
     email: {
         valueMissing: 'El e-mail no puede estar vacío.',
-        typeMismatch: 'El e-mail no es valido.'
+        typeMismatch: 'Ingresa un e-mail válido.'
     },
     asunto: {
         valueMissing: 'El asunto no puede estar vacío.',
-        patternMismatch: 'El asunto solo tiene espacios en blanco.'
+        patternMismatch: 'El asunto solo puede contener letras y espacios.'
     },
     mensaje: {
         valueMissing: 'El mensaje no puede estar vacío.',
-        customError: 'El mensaje solo tiene espacios en blanco.'
+        customError: 'El mensaje solo puede contener letras y espacios.'
     }
-}
+};
 
+// Lista de tipos de errores posibles
 const tipoDeErrores = [
     'valueMissing',
     'patternMismatch',
     'typeMismatch',
     'customError'
-]
+];
 
+// Función para validar el campo "mensaje"
 function validarMensaje(input) {
     const mensaje = input.value;
     let errorMensaje = '';
@@ -73,14 +82,15 @@ function validarMensaje(input) {
     input.setCustomValidity(errorMensaje);
 }
 
+// Función para generar mensajes de error basados en el tipo de campo y el tipo de error
 function mostrarMensajeDeError(tipoDeInput, input) {
     let mensaje = '';
 
     tipoDeErrores.forEach(error => {
         if (input.validity[error]) {
-            mensaje = mensajesDeError[tipoDeInput][error]
+            mensaje = mensajesDeError[tipoDeInput][error];
         }
-    })
+    });
 
-    return mensaje
+    return mensaje;
 }
