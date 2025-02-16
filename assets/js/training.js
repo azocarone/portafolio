60359ff6446f9368336ff747aec0c7e7d10974e7
azocarone <azocarone@gmail.com>
2025-02-16 16:33:15 -0400

    Borrado de archivos

--- assets/js/training.js
+++
@@ -1,6 +0,0 @@
-import { setupImageEvents } from "./imagesEvents.js";
-
-// Configura los eventos de las imágenes cuando el DOM se carga
-document.addEventListener("DOMContentLoaded", () => {
-  setupImageEvents();
-});
