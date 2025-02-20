d39d3b56d2f52e35074b4ccc20242f4f26aed6d0
azocarone <azocarone@gmail.com>
2025-02-20 09:12:03 -0400

    Ajuste al diseñó CSS

--- js/data/contact-validations-error.js
+++ js/data/contact-validations-error.js
@@ -6,7 +6,7 @@ export const mensajesDeError = {
   },
   email: {
     valueMissing: "El e-mail no puede estar vacío.",
-    typeMismatch: "Ingresa un e-mail válido.",
+    typeMismatch: "Ingrese un e-mail válido.",
   },
   asunto: {
     valueMissing: "El asunto no puede estar vacío.",
