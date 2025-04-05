c3e2c2de881178b351ed669d1bf033399342e7c8
azocarone <azocarone@gmail.com>
2025-04-04 23:37:52 -0400

    Prueba implementacion Netlify functions

--- js/contact-ui.js
+++ js/contact-ui.js
@@ -7,31 +7,6 @@ const tipoDeErrores = [
     "customError",
 ];
 
-export function reiniciarFormulario(form, inputs, button) {
-    form.reset();
-    grecaptcha.reset();
-    inputs.forEach((input) => {
-        input.classList.remove("contact__input--invalid");
-        input.parentElement.querySelector(".contact__error").textContent = "";
-    });
-    button.disabled = true;
-    button.classList.remove("contact__button--enabled");
-}
-
-export function mostrarMensaje(tipo, mensaje) {
-    /*const mensajeElement = document.createElement("div");
-    mensajeElement.classList.add("mensaje");
-    mensajeElement.classList.add(`mensaje--${tipo}`);
-    mensajeElement.textContent = mensaje;
-
-    document.body.appendChild(mensajeElement);
-
-    setTimeout(() => {
-        mensajeElement.remove();
-    }, 3000);*/
-    alert(mensaje); // Puedes mejorarlo con una notificación visual.
-}
-
 export function actualizarEstadoDeCampo(input, tipoDeInput, campoInvalidoClass, errorElement) {
     const esValido = input.validity.valid;
     input.classList.toggle(campoInvalidoClass, !esValido);
@@ -46,4 +21,19 @@ export function actualizarEstadoDeCampo(input, tipoDeInput, campoInvalidoClass, 
 function mostrarMensajeDeError(input, tipoDeInput) {
     const error = tipoDeErrores.find((error) => input.validity[error]);
     return error ? mensajesDeError[tipoDeInput][error] : "";
+}
+
+export function reiniciarFormulario(form, inputs, button) {
+    form.reset();
+    grecaptcha.reset();
+    inputs.forEach((input) => {
+        input.classList.remove("contact__input--invalid");
+        input.parentElement.querySelector(".contact__error").textContent = "";
+    });
+    button.disabled = true;
+    button.classList.remove("contact__button--enabled");
+}
+
+export function mostrarMensaje(tipo, mensaje) {
+    alert(mensaje);
 }
