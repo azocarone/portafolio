b07917651ec835e7f991cbd0aaba1a5670c4986f
azocarone <azocarone@gmail.com>
2026-04-08 00:31:12 -0400

    Refactorización del objeto de rutas de imagenes

--- src/data/education-image-data.js
+++ src/data/education-image-data.js
@@ -1,14 +1,11 @@
-export const ImageData = {
-    uc3m: {
-        originalImage: new URL("/assets/img/logos/uc3m.png", import.meta.url).href,
-        hoverImage: new URL("/assets/img/education/uc3m.jpeg", import.meta.url).href,
-    },
-    uah: {
-        originalImage: new URL("/assets/img/logos/uah.png", import.meta.url).href,
-        hoverImage: new URL("/assets/img/education/uah.jpeg", import.meta.url).href,
-    },
-    iunp: {
-        originalImage: new URL("/assets/img/logos/iunp.png", import.meta.url).href,
-        hoverImage: new URL("/assets/img/education/iunp.jpeg", import.meta.url).href,
-    },
-};
+const keys = ["uc3m", "uah", "iunp"];
+
+export const ImageData = Object.fromEntries(
+    keys.map(key => [
+        key, 
+        {
+            originalImage: new URL(`/assets/img/logos/${key}.png`, import.meta.url).href,
+            hoverImage: new URL(`/assets/img/education/${key}.jpeg`, import.meta.url).href,
+        }
+    ])
+);
