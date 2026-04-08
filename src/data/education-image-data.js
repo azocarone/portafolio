39d17252356c76902af310743b11a9ba51bb9b5d
azocarone <azocarone@gmail.com>
2026-04-07 23:57:54 -0400

    Correción de carga de imagenes

--- src/data/education-image-data.js
+++ src/data/education-image-data.js
@@ -1,14 +1,14 @@
 export const ImageData = {
     uc3m: {
-        originalImage: "./assets/img/logos/uc3m.png",
-        hoverImage: "./assets/img/education/uc3m.jpeg",
+        originalImage: "/assets/img/logos/uc3m.png",
+        hoverImage: "/assets/img/education/uc3m.jpeg",
     },
     uah: {
-        originalImage: "./assets/img/logos/uah.png",
-        hoverImage: "./assets/img/education/uah.jpeg",
+        originalImage: "/assets/img/logos/uah.png",
+        hoverImage: "/assets/img/education/uah.jpeg",
     },
     iunp: {
-        originalImage: "./assets/img/logos/iunp.png",
-        hoverImage: "./assets/img/education/iunp.jpeg",
+        originalImage: "/assets/img/logos/iunp.png",
+        hoverImage: "/assets/img/education/iunp.jpeg",
     },
 };
